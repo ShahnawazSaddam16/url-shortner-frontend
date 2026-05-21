3f67a8193b101d28ea1f2cf8b4c165003159a018
ShahanwazSaddam144 <shahnawazsaddamb@gmail.com>
2026-05-22 00:37:09 +0500

    making profile page

---
+++ app/profile/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+
+const Profile = () => {
+  return (
+    <>
+    <Navbar />
+    <section>
+        
+    </section>
+    </>
+  )
+}
+
+export default Profile;
